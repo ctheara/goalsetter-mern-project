523448229d3a21a3c623b736a7b171b9e8c8ec0e
ctheara <ctheara@github.com>
2023-05-27 13:56:59 -0400

    Add goals controller and routes setup

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const goalController = require('../controllers/goalController')
+
+router.route("/").get(goalController.getGoals).post(goalController.setGoals);
+
+router.route("/:id").put(goalController.updateGoals).delete(goalController.deleteGoals);
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
+const { errorHandler } = require("./middleware/errorMiddleware")
 
 const app = express();
 
-app.get("/api/goals", (req, res) => {
-  res.json({message: 'Get goals'})
-});
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+// Routes
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
